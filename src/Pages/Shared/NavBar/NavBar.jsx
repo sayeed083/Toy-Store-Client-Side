43946f76cb1aa76d339a557575d3a6bd47e4e5bf
sayeed083 <sayeedislam407@gmailcom>
2023-05-18 00:23:57 +0600

    Meaningful NavSection Created With Specific Requirements

--- src/Pages/Shared/NavBar/NavBar.jsx
+++ src/Pages/Shared/NavBar/NavBar.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const NavBar = () => {
     return (
         <div>
-            <div className="navbar px-4 bg-purple-400">
+            <div className="navbar px-9 bg-purple-400">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <label tabIndex={0} className="btn btn-ghost lg:hidden">
